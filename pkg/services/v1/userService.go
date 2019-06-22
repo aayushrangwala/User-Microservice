a4c12bf6af66c137c749617faeafbf6a800134bb
Ayush Rangwala <aayushrangwala@gmail.com>
2019-06-22 13:06:56 +0530

    Added one more handler

--- pkg/services/v1/userService.go
+++ pkg/services/v1/userService.go
@@ -31,16 +31,17 @@ func GetMicroserviceName(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	err := json.NewEncoder(w).Encode("User Microservice")
 	if err != nil {
-		log.Fatal("Error encoding or returning the value", err)
+		log.Fatal("Error encoding or returning the response", err)
 	}
 }
 
 // GetUserEmail returns the email of the user
 func GetUserEmail(w http.ResponseWriter, r *http.Request) {
 	userEmail := "ex@ex.com"
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Content-Type", "plain/text; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(userEmail); err != nil {
-		panic(err)
+	_, err := w.Write([]byte(userEmail))
+	if err != nil {
+		log.Fatal("Error wrting or returning the response", err)
 	}
 }
